7102674f932623ef2364390a655d3121ba933b19
smallTigerBrother <664674103@qq.com>
2015-08-15 17:10:01 +0800

    新增WebViewActivity

--- HiRemote/app/src/main/java/com/lepow/hiremote/misc/IntentKeys.java
+++ HiRemote/app/src/main/java/com/lepow/hiremote/misc/IntentKeys.java
@@ -6,4 +6,17 @@ public class IntentKeys
 	 * 传递LocationInfo的键值
 	 */
 	public static final String LOCATION_INFO = "location_info";
+
+	/**
+	 * WebViewActivity显示的页面的URL地址
+	 */
+	public static final String URL = "url";
+
+	/**
+	 * WebViewActivity的标题
+	 */
+	public static final String WEBVIEW_ACTIVITY_TITLE = "bar_title";
+
+	public static final String DEVICE_INFO = "device_info";
+
 }
