ee195b847110eee0d517623a5998687da14034c8
SmallTigerBrother <664674103@qq.com>
2015-11-10 20:36:29 +0800

    修改Google地址解析方法，修改编译版本，修改兼容库版本

--- HiRemote/app/src/main/java/com/mn/tiger/location/GoogleGeoCoding.java
+++ HiRemote/app/src/main/java/com/mn/tiger/location/GoogleGeoCoding.java
@@ -1,16 +1,8 @@
 package com.mn.tiger.location;
 
-import com.google.gson.Gson;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.concurrent.Executors;
+import com.mn.tiger.app.TGApplication;
+import com.mn.tiger.request.TGHttpLoader;
+import com.mn.tiger.request.receiver.TGHttpResult;
 
 /**
  * Google地址解析功能
@@ -26,46 +18,43 @@ public class GoogleGeoCoding
 	public static void geoCoding(final double latitude, final double longitude,
 			final GeoCodeListener listener)
 	{
-		Executors.newCachedThreadPool().execute(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				String url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude
-						+ "," + longitude + "&sensor=false";
-				// 创建一个HttpClient对象
-				HttpClient httpClient = new DefaultHttpClient();
-				String responseData = "";
-				try
-				{
-					// 向指定的URL发送Http请求
-					HttpResponse response = httpClient.execute(new HttpGet(url));
-					// 取得服务器返回的响应
-					HttpEntity entity = response.getEntity();
-					BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(entity
-							.getContent()));
-					String line = "";
-					while ((line = bufferedReader.readLine()) != null)
-					{
-						responseData = responseData + line;
-					}
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-					if (null != listener)
-					{
-						listener.onGeoCodingError(0, e.getMessage());
-					}
-				}
+        String url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude
+                + "," + longitude + "&sensor=false";
 
-				if (null != listener)
-				{
-					listener.onGeoCodingSuccess(new Gson().fromJson(responseData,
-							GeoCodeResult.class));
-				}
-			}
-		});
+        TGHttpLoader<GeoCodeResult> httpLoader = new TGHttpLoader<GeoCodeResult>();
+        httpLoader.loadByGet(TGApplication.getInstance(), url, GeoCodeResult.class,
+                new TGHttpLoader.OnLoadCallback<GeoCodeResult>()
+                {
+                    @Override
+                    public void onLoadStart()
+                    {
+                    }
+
+                    @Override
+                    public void onLoadSuccess(GeoCodeResult geoCodeResult, TGHttpResult tgHttpResult)
+                    {
+                        listener.onGeoCodingSuccess(geoCodeResult);
+                    }
+
+                    @Override
+                    public void onLoadError(int i, String s, TGHttpResult tgHttpResult)
+                    {
+                        if (null != listener)
+                        {
+                            listener.onGeoCodingError(i, s);
+                        }
+                    }
+
+                    @Override
+                    public void onLoadCache(GeoCodeResult geoCodeResult, TGHttpResult tgHttpResult)
+                    {
+                    }
+
+                    @Override
+                    public void onLoadOver()
+                    {
+                    }
+                });
 	}
 
 	/**
